f185add3347c246fcba28e73e46ed37dbe9e257b
jamesapowers <powersajames@gmail.com>
2018-02-02 20:07:42 -0800

    Updates $http params to include type:video and videoEmbed: true

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -8,7 +8,9 @@ angular.module('video-player')
           q: string,
           key: window.YOUTUBE_API_KEY,
           maxResults: 5,
-          part: 'snippet'
+          part: 'snippet',
+          type: 'video',
+          videoEmbeddable: 'true'
         },
         
         url: 'https://www.googleapis.com/youtube/v3/search'
